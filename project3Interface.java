ab8a55dbb6ddaba9ac322671f45ba3b972045ef0
Savannah <savannah_designs@icloud.com>
2025-04-18 22:13:02 -0700

    Create project3Interface.java

---
+++ project3Interface.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * @author savannah
+ */
+public interface project3Interface {
+    public char charAt(int index);
+    public LinkedString concat(LinkedString str);
+    public boolean isEmpty();
+    public int length();
+    public LinkedString substring(int beginIndex, int endIndex);
+}
